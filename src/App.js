d201332e19483457c036c2daf386ab167542ce28
dev-deepesh <121635857+dev-deepesh@users.noreply.github.com>
2023-07-15 19:13:21 +0530

    Add Theme

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import NoteLayout from './components/NoteLayout/NoteLayout';
 import Header from './components/Header/Header';
+import ThemeProvider from '@material-ui/core'
+import theme from './theme';
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <NoteLayout/>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Header/>
+        <NoteLayout/>
+      </div>
+    </ThemeProvider>
   );
 }
 
